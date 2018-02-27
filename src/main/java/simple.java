cf3ce39f6837c1f914678ffbf6f8e77ce3fdd761
Neelesh-Bansal <neelesh.bansal@tothenew.com>
2018-02-27 20:50:24 +0530

    Gradle_Exercise

---
+++ src/main/java/simple.java
@@ -0,0 +1,8 @@
+
+import org.apache.commons.lang3.*;
+public class simple {
+
+    public static void main(String[] args) {
+        System.out.println(StringUtils.swapCase("Hello World"));
+    }
+}
